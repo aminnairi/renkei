13cdc0be156d2d05cabeeb0651f004e64f162d2d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-27 15:29:41 +0100

    added material ui

--- example/react/src/App.tsx
+++ example/react/src/App.tsx
@@ -2,6 +2,19 @@ import { ChangeEventHandler, FormEventHandler, useCallback, useState } from 'rea
 import { createClient } from "@superblue/example-core";
 import { Users } from '@superblue/example-core/routes/getUsers';
 
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Alert from "@mui/material/Alert";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Container from "@mui/material/Container";
+
 const client = createClient({ server: "http://localhost:8000" });
 
 function App() {
@@ -49,34 +62,65 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h2>Create one user</h2>
-      <small>{message}</small>
-      <small style={{ color: "red" }}>{error}</small>
-      <form onSubmit={createUser}>
-        <input type="text" placeholder="First Name" value={firstname} onChange={updateFirstname} />
-        <input type="text" placeholder="Last Name" value={lastname} onChange={updateLastname} />
-        <button>Create</button>
-      </form>
-      <h2>Users</h2>
-      <button onClick={getUsers}>Refresh</button>
-      {users.length === 0 && (
-        <p>No users available.</p>
-      )}
-      {users.length !== 0 && (
-        <table>
-          <tbody>
-            {users.map(user => (
-              <tr key={user.identifier}>
-                <td>{user.identifier}</td>
-                <td>{user.firstname}</td>
-                <td>{user.lastname}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </>
+    <Container>
+      <Stack spacing={3}>
+        <Typography variant="h4" align="center">
+          Create one user
+        </Typography>
+        {message && (
+          <Alert severity="info">
+            {message}
+          </Alert>
+        )}
+        {error && (
+          <Alert severity="error">
+            {error}
+          </Alert>
+        )}
+        <Stack component="form" onSubmit={createUser} spacing={3}>
+          <TextField label="First Name" type="text" value={firstname} onChange={updateFirstname} />
+          <TextField type="text" label="Last Name" value={lastname} onChange={updateLastname} />
+          <Button variant="contained" sx={{ alignSelf: "center" }} type="submit">
+            Create
+          </Button>
+        </Stack>
+        <Typography variant="h4" align="center">Users</Typography>
+        <Button onClick={getUsers} variant="outlined" sx={{ alignSelf: "center" }}>
+          Refresh
+        </Button>
+        {users.length === 0 && (
+          <Typography align="center">No users available.</Typography>
+        )}
+        {users.length !== 0 && (
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    Identifier
+                  </TableCell>
+                  <TableCell>
+                    First Name
+                  </TableCell>
+                  <TableCell>
+                    Last Name
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map(user => (
+                  <TableRow key={user.identifier}>
+                    <TableCell>{user.identifier}</TableCell>
+                    <TableCell>{user.firstname}</TableCell>
+                    <TableCell>{user.lastname}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </Stack>
+    </Container>
   )
 }
 
