ecd3a22a9ca9e8622362b839c8ab1c05f7a63737
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-27 15:17:43 +0100

    exported the implementations in their own files

--- example/node/index.ts
+++ example/node/index.ts
@@ -1,28 +1,13 @@
-import { createImplementation, createHandler } from "@superblue/example-core";
+import { createHandler } from "@superblue/example-core";
 import { createServer } from "http";
-
-const createUserImplementation = createImplementation({
-  route: "createUser",
-  implementation: async ({ firstname }) => {
-    if (Math.random() > 0.5) {
-      return {
-        success: false,
-        error: `Random error when creating ${firstname}`
-      };
-    }
-
-    return {
-      success: true,
-      message: `Created ${firstname}`
-    }
-  }
-});
-
+import { createUserImplementation } from "./implementations/createUser";
+import { getUsersImplementation } from "./implementations/getUsers";
 
 const handler = createHandler({
   clients: ["http://localhost:5173"],
   implementations: {
-    createUser: createUserImplementation
+    createUser: createUserImplementation,
+    getUsers: getUsersImplementation
   }
 })
 
