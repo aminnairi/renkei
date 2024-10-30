0f534bf2d74dc04ae298f0b7c76a69ce8d431b65
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-30 12:29:14 +0100

    renamed handler to request listener for a closer match on the http builtin node library

--- example/node/index.ts
+++ example/node/index.ts
@@ -1,10 +1,10 @@
-import { createHandler } from "@superblue/example-core";
+import { createRequestListener } from "@superblue/example-core";
 import { createServer } from "http";
 import { createUserImplementation } from "./implementations/createUser";
 import { getUsersImplementation } from "./implementations/getUsers";
 import { userCreatedImplementation } from "./implementations/userCreated";
 
-const handler = createHandler({
+const requestListener = createRequestListener({
   clients: ["http://localhost:5173"],
   implementations: {
     createUser: createUserImplementation,
@@ -13,7 +13,7 @@ const handler = createHandler({
   }
 })
 
-const server = createServer(handler);
+const server = createServer(requestListener);
 
 server.listen(8000, "0.0.0.0", () => {
   console.log("Server listening on http://localhost:8000");
