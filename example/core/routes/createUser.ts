0fd734911c54b2593722d20c3e579677dc71a817
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-30 21:41:13 +0100

    updated the way errors are handled to include a new error for limited requests

--- example/core/routes/createUser.ts
+++ example/core/routes/createUser.ts
@@ -5,20 +5,29 @@ export const createUserRoute = createHttpRoute({
     firstname: z.string(),
     lastname: z.string()
   }),
-  response: z.discriminatedUnion("success", [
+  response: z.discriminatedUnion("status", [
     z.object({
-      success: z.literal(true),
+      status: z.literal("SUCCESS"),
       message: z.string()
     }),
     z.object({
-      success: z.literal(false),
-      error: z.union([
-        z.literal("FIRSTNAME_EMPTY"),
-        z.literal("LASTNAME_EMPTY"),
-        z.literal("FIRSTNAME_TOO_LONG"),
-        z.literal("LASTNAME_TOO_LONG"),
-        z.literal("USER_ALREADY_EXISTS")
-      ])
+      status: z.literal("LIMITED"),
+      retryAfter: z.number(),
+    }),
+    z.object({
+      status: z.literal("FIRSTNAME_EMPTY")
+    }),
+    z.object({
+      status: z.literal("LASTNAME_EMPTY")
+    }),
+    z.object({
+      status: z.literal("FIRSTNAME_TOO_LONG")
+    }),
+    z.object({
+      status: z.literal("LASTNAME_TOO_LONG")
+    }),
+    z.object({
+      status: z.literal("USER_ALREADY_EXISTS")
     })
   ])
 })
