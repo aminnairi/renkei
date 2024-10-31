6a9c3b853ece3f16bfa31055d25d0485dbb7f6c1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-31 14:29:49 +0100

    initial commit

---
+++ example/react/src/components/header.tsx
@@ -0,0 +1,15 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h4">
+          Superblue
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
