729e94b01ad2cfb4a1cba85732a6889bb1588dd5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-11-01 20:10:02 +0100

    using the new form of implementation for server routes

--- example/node/implementations/createUser.ts
+++ example/node/implementations/createUser.ts
@@ -1,77 +1,58 @@
-import { createHttpImplementation } from "@superblue/example-core";
 import { randomUUID } from "crypto";
 import { users } from "../state/users";
 import { userCreatedEvent } from "../events/userCreatedEvent";
-import { limit } from "../limiter";
+import { implementCreateUser } from "../../shared/routes/createUser";
 
-export const createUserImplementation = createHttpImplementation({
-  route: "createUser",
-  implementation: async ({ firstname, lastname }, { headers: { origin } }) => {
-    const limitation = await limit(origin ?? "http://unknown");
+export const createUserImplementation = implementCreateUser(async ({ firstname, lastname }) => {
+  const trimmedFirstname = firstname.trim();
+  const trimmedLastname = lastname.trim();
 
-    if (limitation instanceof Error) {
-      return {
-        status: "UNEXPECTED_ERROR"
-      }
-    }
-
-    if (limitation.status === "limited") {
-      return {
-        status: "LIMITED",
-        retryAfter: limitation.unlockedAt
-      };
-    }
-
-    const trimmedFirstname = firstname.trim();
-    const trimmedLastname = lastname.trim();
-
-    if (trimmedFirstname.length === 0) {
-      return {
-        status: "FIRSTNAME_EMPTY"
-      };
-    }
-
-    if (trimmedLastname.length === 0) {
-      return {
-        status: "LASTNAME_EMPTY"
-      };
-    }
-
-    if (trimmedFirstname.length > 50) {
-      return {
-        status: "FIRSTNAME_TOO_LONG"
-      };
-    }
-
-    if (trimmedLastname.length > 50) {
-      return {
-        status: "LASTNAME_TOO_LONG"
-      };
-    }
-
-    const alreadyExistingUser = users.find(user => {
-      return user.firstname === trimmedFirstname && user.lastname === trimmedLastname;
-    });
-
-    if (alreadyExistingUser) {
-      return {
-        status: "USER_ALREADY_EXISTS"
-      };
-    }
-
-    const user = {
-      identifier: randomUUID(),
-      firstname: trimmedFirstname,
-      lastname: trimmedLastname
-    };
-
-    users.push(user);
-
-    userCreatedEvent.emit("user", user);
-
+  if (trimmedFirstname.length === 0) {
     return {
-      status: "SUCCESS",
-      message: `Created ${firstname}`
-    }
+      status: "FIRSTNAME_EMPTY"
+    };
+  }
+
+  if (trimmedLastname.length === 0) {
+    return {
+      status: "LASTNAME_EMPTY"
+    };
+  }
+
+  if (trimmedFirstname.length > 50) {
+    return {
+      status: "FIRSTNAME_TOO_LONG"
+    };
+  }
+
+  if (trimmedLastname.length > 50) {
+    return {
+      status: "LASTNAME_TOO_LONG"
+    };
+  }
+
+  const alreadyExistingUser = users.find(user => {
+    return user.firstname === trimmedFirstname && user.lastname === trimmedLastname;
+  });
+
+  if (alreadyExistingUser) {
+    return {
+      status: "USER_ALREADY_EXISTS"
+    };
+  }
+
+  const user = {
+    identifier: randomUUID(),
+    firstname: trimmedFirstname,
+    lastname: trimmedLastname
+  };
+
+  users.push(user);
+
+  userCreatedEvent.emit("user", user);
+
+  return {
+    status: "SUCCESS",
+    message: `Created ${firstname}`
   }
 });
