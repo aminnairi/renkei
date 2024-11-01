a8500897d6f887f6adaa91a47fb03ffbbb0d96b0
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-11-01 20:10:38 +0100

    moved in the shared package

--- example/core/routes/userCreated.ts
+++
@@ -1,9 +0,0 @@
-import { createEventRoute, z } from "@superblue/core";
-
-export const userCreatedRoute = createEventRoute({
-  response: z.object({
-    identifier: z.string(),
-    firstname: z.string(),
-    lastname: z.string()
-  })
-});
