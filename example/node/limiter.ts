1f3775c9fbc41fc96741c710068b7057530e6eef
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-11-01 20:47:33 +0100

    removed waiting for better integration

--- example/node/limiter.ts
+++
@@ -1,8 +0,0 @@
-import { createLimiter, InMemoryStrategy } from "@superblue/limiter";
-
-// 1 request per 10 seconds
-export const limit = createLimiter({
-  requestsPerWindow: 1,
-  windowInSeconds: 10,
-  strategy: new InMemoryStrategy
-});
